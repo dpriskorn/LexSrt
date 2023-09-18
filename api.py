9baf87a8374e9617d618bf3edde4055e27732c0a
Dennis Priskorn <egils@riseup.net>
2023-09-18 17:32:55 +0200

    feat: New TokenResponse class to avoid mypy complaint

--- api.py
+++ api.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel
 from starlette.responses import JSONResponse
 
 from models.srt_sentence import SrtSentence
+from models.token_response import TokenResponse
 
 app = FastAPI()
 
@@ -13,13 +14,10 @@ class SentenceRequest(BaseModel):
     sentence: str
     spacy_model: str
 
-class IndividualResponse(BaseModel):
-    token: str
-    spacy_pos: str
-    matched_forms: List[str]
 
 class ResponseList(BaseModel):
-    data: List[IndividualResponse]
+    data: List[TokenResponse]
+
 
 @app.post("/process_sentence", response_model=List[str])
 async def process_sentence(sentence_request: SentenceRequest):
@@ -29,9 +27,12 @@ async def process_sentence(sentence_request: SentenceRequest):
         )
         srt_sentence.clean_get_tokens_and_extract_forms()
         if srt_sentence.number_of_tokens:
-            response_list = [IndividualResponse(**item) for item in srt_sentence.get_dictionaries]
-            return JSONResponse(content=ResponseList(data=response_list).model_dump())
+            return JSONResponse(
+                content=ResponseList(data=srt_sentence.get_token_responses).model_dump()
+            )
         else:
-            raise HTTPException(status_code=400, detail="Error: No tokens found by spaCy")
+            raise HTTPException(
+                status_code=400, detail="Error: No tokens found by spaCy"
+            )
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
