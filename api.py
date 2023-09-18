cc9afa505a4e951f29d3159f37f4859e7b6f390f
Dennis Priskorn <egils@riseup.net>
2023-09-18 17:21:44 +0200

    chore: Rename class

--- api.py
+++ api.py
@@ -18,7 +18,7 @@ class IndividualResponse(BaseModel):
     spacy_pos: str
     matched_forms: List[str]
 
-class MyResponseList(BaseModel):
+class ResponseList(BaseModel):
     data: List[IndividualResponse]
 
 @app.post("/process_sentence", response_model=List[str])
@@ -30,7 +30,7 @@ async def process_sentence(sentence_request: SentenceRequest):
         srt_sentence.clean_get_tokens_and_extract_forms()
         if srt_sentence.number_of_tokens:
             response_list = [IndividualResponse(**item) for item in srt_sentence.get_dictionaries]
-            return JSONResponse(content=MyResponseList(data=response_list).model_dump())
+            return JSONResponse(content=ResponseList(data=response_list).model_dump())
         else:
             raise HTTPException(status_code=400, detail="Error: No tokens found by spaCy")
     except Exception as e:
